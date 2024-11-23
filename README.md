7cc68f334090640b7f3f74422b9d12b16be2a1dd
uniras <tkappeng@gmail.com>
2024-11-24 05:26:14 +0900

    グローバル変数対応

--- README.md
+++ README.md
@@ -58,6 +58,18 @@ options = {
 }
 ```
 
+### グローバル変数
+
+PyScriptから以下の変数にアクセスできます。
+
+- 別のセルで設定したグローバル変数(_で始まる変数名やJSONに変換できないものは除く)
+- マジックコマンドの引数py_valで設定した変数
+- width: iframeの幅(マジックコマンドの引数で指定した幅)
+- height: iframeの高さ(マジックコマンドの引数で指定した高さ)
+
+この変数はjs.pysオブジェクトを介してアクセスできます。
+変数名が衝突した場合は上記リストの順に上書きされて適用されます。
+
 ### マジックコマンド
 
 #### %%runss
@@ -65,13 +77,14 @@ options = {
 セル内のjspreadsheet.jsを使ったPythonコードをPyScriptを用いてiframe内で実行するマジックコマンド
 
 ```juypyter
-%%runss [width] [height] [background] [py_type] [py_conf] [js_src] [py_ver]
+%%runss [width] [height] [background] [py_type] [py_val] [py_conf] [js_src] [py_ver]
 ```
 
 - width: iframeの幅を指定します。デフォルトは500です。
 - height: iframeの高さを指定します。デフォルトは500です。
 - background: iframeの背景色を指定します。デフォルトはwhiteです。
 - py_type: 実行するPythonの種類。pyまたはmpyを指定します。mpyはMicroPyton、pyはCPython互換のPyodideで実行します。デフォルトはmpyです。グローバルモードのときはmpy固定です。
+- py_val: PyScriptの変数を''で囲んだJSON形式で指定します。デフォルトは'{}'です。
 - py_conf: PyScriptの設定を''で囲んだJSON形式で指定します。デフォルトは{}です。
 - js_src: 外部JavaScriptのURLを''で囲んだ文字列のJSON配列形式で指定します。デフォルトは[]です。
 - py_ver: PyScriptのバージョンを指定します.
--- spreadmagic/spreadmagic.py
+++ spreadmagic/spreadmagic.py
@@ -19,15 +19,16 @@ def runss(line, cell):
     セル内のPythonコードをPyScriptを用いてiframe内で実行するマジックコマンド
 
     Usage:
-        %%runss [width] [height] [background] [py_type] [py_conf] [js_src] [version]
+        %%runss [width] [height] [background] [py_type] [py_val] [py_conf] [js_src] [version]
 
     Args:
         width: iframeの幅を指定します。デフォルトは500です。
         height: iframeの高さを指定します。デフォルトは500です。
         background: iframeの背景色を指定します。デフォルトはwhiteです。
         py_type: 実行するPythonの種類。pyまたはmpyを指定します。pyはCPython互換のPyodide、mpyはMicroPytonで実行します。デフォルトはmpyです。
-        py_conf: PyScriptの設定を''で囲んだJSON形式で指定します。デフォルトは{}です。
-        js_src: 外部JavaScriptのURLを''で囲んだ文字列のJSON配列形式で指定します。デフォルトは[]です。
+        py_val: PyScriptの変数を''で囲んだJSON形式で指定します。デフォルトは'{}'です。
+        py_conf: PyScriptの設定を''で囲んだJSON形式で指定します。デフォルトは'{}'です。
+        js_src: 外部JavaScriptのURLを''で囲んだ文字列のJSON配列形式で指定します。デフォルトは'[]'です。
         version: PyScriptのバージョンを指定します.
     """
     args = parse_spread_args(line, cell)
@@ -60,9 +61,10 @@ def parse_spread_args(line, cell):
     args["height"] = line_args[1] if len(line_args) > 1 else "500"
     args["background"] = line_args[2] if len(line_args) > 2 else "white"
     args["py_type"] = line_args[3].lower() if len(line_args) > 3 else "mpy"
-    args["py_conf"] = line_args[4] if len(line_args) > 4 and line_args[4] != "{}" else None
-    args["js_src"] = line_args[5] if len(line_args) > 5 and line_args[5] != "[]" else None
-    args["py_ver"] = line_args[6] if len(line_args) > 6 else "none"
+    args["py_val"] = line_args[4] if len(line_args) > 4 and line_args[4] != "{}" else None
+    args["py_conf"] = line_args[5] if len(line_args) > 5 and line_args[5] != "{}" else None
+    args["js_src"] = line_args[6] if len(line_args) > 6 and line_args[6] != "[]" else None
+    args["py_ver"] = line_args[7] if len(line_args) > 7 else "none"
     args["py_script"] = cell
 
     return args
